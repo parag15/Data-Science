544b59b423cbd396a6755e83954e554b8fb122a7
parag15 <61828910+parag15@users.noreply.github.com>
2020-03-09 14:03:39 +0530

    Add files via upload

---
+++ Capstone-project.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera Data Science Capstone Project
+# ## This notebook will be used for the capstone project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project")
+
+
+# In[ ]:
+
+
+
+
