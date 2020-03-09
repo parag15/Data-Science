289f07db0b8526207cd7d30bef9bbbac91d0b021
parag15 <parag.a.deshpande@outlook.com>
2020-03-09 13:41:13 +0530

    adding capstone project python file

--- Capstone Project.py
+++ Capstone Project.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
+
+
+# Capstone Project Notebook
+## This notebook will be used for the Coursera Data Science Capstone project
+
+
+# In[2]:
 
 
 import pandas as pd
